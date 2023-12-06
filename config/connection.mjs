546d798c342e72d01afa35179717b3daf3d1706e
anrwn <andrenrwn@users.noreply.github.com>
2023-12-06 12:31:35 +0800

    Bare bones to start developing

--- config/connection.mjs
+++ config/connection.mjs
@@ -2,14 +2,16 @@
 import mysql from 'mysql2';
 
 // Connect to database
-export const db = mysql.createConnection(
+const db = mysql.createConnection(
   {
     host: 'localhost',
     // MySQL username,
     user: 'root',
     // TODO: Add MySQL password here
     password: 'mysql',
-    database: 'employees_db'
+    database: 'employeetracker_db'
   },
   console.log(`Connected to the employees_db database.`)
 );
+
+export default db;
