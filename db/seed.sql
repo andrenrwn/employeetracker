8b37765148a9f34fc3a13fdeb6396b15b4d71a28
anrwn <andrenrwn@users.noreply.github.com>
2023-12-09 04:02:45 +0800

    Added view depts,rols,employees&by mgr, add dept,role,employee, update role, manager. Refactored most functions

--- db/seed.sql
+++ db/seed.sql
@@ -15,25 +15,29 @@ VALUES
 (4, "Software Engineer", 120000, 2),
 (5, "Account Manager", 160000, 3),
 (6, "Accountant", 125000, 3),
-(7, "Legal Team Lead", 250000, 1),
-(8, "Lawyer", 190000, 1);
+(7, "Legal Team Lead", 250000, 4),
+(8, "Lawyer", 190000, 4);
 
 INSERT INTO employee (id, first_name, last_name, role_id)
 VALUES 
 (1, "John", "Doe", 1),
-(2, "Mike", "Chan", 1),
-(3, "Ashley", "Rodriguez", 1),
+(2, "Mike", "Chan", 2),
+(3, "Ashley", "Rodriguez", 2),
 (4, "Kevin", "Tupik", 4),
 (5, "Kunal", "Singh", 5),
 (6, "Malia", "Brown", 6),
 (7, "Sarah", "Lourd", 7),
-(8, "Tom", "Allen", 8);
+(8, "Tom", "Allen", 8),
+(9, "Jane", "Day", 4),
+(10, "Tom", "Jerry", 3),
+(11, "Edsgar", "Djikstra", 3);
 
 INSERT INTO hierarchy (id, manager_id)
 VALUES
 (2, 1),
+(3, 1),
 (4, 3),
 (6, 5),
-(8, 7);
-
-﻿
+(8, 7),
+(9,10),
+(9,11);
--- index.js
+++ index.js
@@ -3,17 +3,17 @@ Employee Tracker
 A command-line employeetracker_db MySQL database manager
 *******************************************************/
 
-import { input, select, Separator } from '@inquirer/prompts';
+import { input, select, confirm, Separator } from '@inquirer/prompts';
 import { promises as fs } from 'fs';
 import { getSQLQuery, getColumnNames, getAllRows, displayTable, displaySQLQueryTable } from './lib/functions.mjs';
-import { ShowDepartments, ShowRoles, ShowEmployees } from './lib/tabledisplay.mjs';
+import { ETDepartments, ETRoles, ETEmployees } from './lib/tabledisplay.mjs';
 import db from './config/connection.mjs';
 import Table from 'cli-table';
+import colors from 'colors';
 
-
-let DepartmentTable = new ShowDepartments();
-let RoleTable = new ShowRoles();
-let EmployeeTable = new ShowEmployees();
+let DepartmentTable = new ETDepartments();
+let RoleTable = new ETRoles();
+let EmployeeTable = new ETEmployees();
 
 /* Menu options
 view all departments
@@ -27,7 +27,7 @@ update employee role
 
 // Define menu selections
 let choices = [
-    new Separator(), // common sections in README files
+    new Separator(), // View
     {
         name: "View Departments",
         value: "view_departments",
@@ -44,6 +44,22 @@ let choices = [
         description: "View all employees",
     },
     {
+        name: "View Employees by Manager",
+        value: "view_employees_by_manager",
+        description: "View employees by manager",
+    },
+    {
+        name: "View Employees of a Manager",
+        value: "view_employees_of_manager",
+        description: "View employees managed by a manager",
+    },
+    {
+        name: "View Employee Budgets of Departments",
+        value: "view_salaries_by_department",
+        description: "View combined salaries of all employees by department",
+    },
+    new Separator(), // Add
+    {
         name: "Add Department",
         value: "add_department",
         description: "Add a new department",
@@ -58,23 +74,18 @@ let choices = [
         value: "add_employee",
         description: "Add an employee",
     },
+    new Separator(), // Update
     {
         name: "Update Employee Role",
         value: "update_employee_role",
         description: "Update an employee's role",
     },
-    new Separator(), // uncommon sections
     {
         name: "Update Employee Manager",
         value: "update_employee_manager",
         description: "Update an employee's manager",
     },
-    {
-        name: "View Employees by Manager",
-        value: "view_employees_by_manager",
-        description: "View employees by manager",
-    },
-    new Separator(), // uncommon sections
+    new Separator(), // Delete
     {
         name: "Delete Department",
         value: "delete_department",
@@ -92,21 +103,273 @@ let choices = [
     },
     new Separator(),
     {
-        name: "View Employee Budgets of Departments",
-        value: "view_salaries_by_department",
-        description: "View combined salaries of all employees by department",
-    },
-    new Separator(),
-    {
         name: "quit",
         value: "quit",
         description: "Quit program",
     },
 ];
 
-// a while loop menu for user to select options to add to the logo
+// Put tasks in functions to localize scoped variables.
+
+// Add a new department
+async function add_department() {
+    // display department
+    // console.log("DEPARTMENT");
+    // await displaySQLQueryTable(`SELECT * FROM department ORDER BY department.id`, "", ['Department Id', 'Department']);
+    console.log("\nCURRENT DEPARTMENTS".inverse);
+    await DepartmentTable.show();
+
+    const newdeptname = await input({ message: 'Enter new department name' });
+
+    // We already have a UNIQUE constraint to the query, but check manually anyway for a better error message
+
+    if (await DepartmentTable.exists(newdeptname)) {
+        console.log(`Error: ${newdeptname} department already exists`.error);
+    } else {
+        if (await DepartmentTable.add(newdeptname)) {
+            console.log("\nREFRESHED DEPARTMENT LIST".inverse);
+            await DepartmentTable.show();
+            console.log(`Added ${newdeptname}`.yellow.inverse);
+        };
+    };
+
+    // display department
+    // console.log("DEPARTMENT");
+    // await displaySQLQueryTable(`SELECT * FROM department ORDER BY department.id`, "", ['Department Id', 'Department']);
+};
+
+// Add a new role
+async function add_role() {
+    console.log("\nCURRENT ROLES".inverse);
+    await RoleTable.show();
+
+    const newroletitle = await input({ message: 'Enter new role title: ' });
+    const newrolesalary = await input({ message: 'Enter a salary for the new role: ' });
+
+    // Get department selection
+    let deptchoices = await DepartmentTable.getrows();
+    let deptchoice = await select(
+        {
+            message: "Select a department for the new role:\n",
+            choices: deptchoices,
+            pageSize: deptchoices.length,
+            loop: true,
+        });
+    if (await RoleTable.exists(newroletitle, deptchoice)) {
+        console.log(`Error: Role title ${newroletitle} already exists in the ${(deptchoices.find((elem) => elem.value === deptchoice)).name} department`.red.inverse);
+    } else {
+        if (await RoleTable.add(newroletitle, newrolesalary, deptchoice)) {
+            console.log("\nREFRESHED ROLES".inverse);
+            await RoleTable.show();
+            console.log(`Added ${newroletitle} with a salary of ${newrolesalary} to ${deptchoice}`.yellow.inverse);
+        };
+    };
+};
+
+async function add_employee() {
+    console.log("\nCURRENT EMPLOYEES".inverse);
+    await EmployeeTable.show();
+
+    const firstname = await input({ message: "Enter the employee's first name: " });
+    const lastname = await input({ message: "Enter the employee's last name: " });
+
+    // Get department selection
+    let rolechoices = await RoleTable.getrows();
+    let rolechoice = await select(
+        {
+            message: "Select a department for the new role:\n",
+            choices: rolechoices,
+            pageSize: rolechoices.length,
+            loop: true,
+        });
+    if (await EmployeeTable.exists(firstname, lastname, rolechoice)) {
+        console.log(`Error: Employee ${firstname} ${lastname} in ${((rolechoices.find((elem) => elem.value === rolechoice))).description} already exists`.red.inverse);
+    } else {
+        if (await EmployeeTable.add(firstname, lastname, rolechoice)) {
+            console.log("\nREFRESHED EMPLOYEE LIST".inverse);
+            await EmployeeTable.show();
+            console.log(`Added ${firstname} ${lastname} as ${(rolechoices.find((elem) => elem.value === rolechoice)).description}`.yellow.inverse);
+        };
+    };
+};
+
+// Update employee role.
+async function update_employee_role() {
+    console.log("\nCURRENT EMPLOYEE LIST".inverse);
+    await EmployeeTable.show();
+
+    let employeechoices = await EmployeeTable.getrows();
+    let employeechoice = await select(
+        {
+            message: "Select an employee to update to the new role:\n",
+            choices: employeechoices,
+            pageSize: employeechoices.length,
+            loop: true,
+        });
+    let chosenemployee = employeechoices.find((elem) => { return elem.value === employeechoice });
+
+    let rolechoices = await RoleTable.getrows();
+    let rolechoice = await select(
+        {
+            message: `Select a new role for employee ${chosenemployee.description}:\n`,
+            choices: rolechoices,
+            pageSize: rolechoices.length,
+            loop: true,
+        });
+    let chosenrole = rolechoices.find((elem) => elem.value === rolechoice);
+
+    console.log("CHOSEN EMPLOYEE:", employeechoice, chosenemployee);
+    console.log("CHOSEN ROLE:", rolechoice, chosenrole);
+
+    let employeeexists = await EmployeeTable.exists(chosenemployee.first_name, chosenemployee.last_name, rolechoice);
+    console.log("employee exists:", employeeexists);
+    if (employeeexists) {
+        console.log(`Error: Employee ${chosenemployee.first_name} ${chosenemployee.last_name} in ${chosenrole.description} already exists`.red.inverse);
+    } else {
+        console.log("trying calling updaterole with", employeechoice, rolechoice);
+
+        let updateresult;
+        try {
+            updateresult = await EmployeeTable.updaterole(employeechoice, rolechoice);
+        } catch (err) {
+            console.log(`Error updating result with `.red, result);
+        };
+        if (updateresult) {
+            console.log("\nREFRESHED EMPLOYEE LIST".inverse);
+            await EmployeeTable.show();
+            console.log(`Updated ${chosenemployee.first_name} ${chosenemployee.last_name} to ${chosenrole.description}`.yellow.inverse);
+        };
+    };
+};
+
+// Let the user choose an employee
+let chooseanemployee = async function (promptmessage = "Select an employee\n") {
+    let employeechoices = await EmployeeTable.getrows();
+    let employeechoice = await select(
+        {
+            message: promptmessage,
+            choices: employeechoices,
+            pageSize: employeechoices.length,
+            loop: true,
+        });
+    // console.log(employeechoices); // debug log
+    let chosenemployee = employeechoices.find((elem) => { return elem.value === employeechoice });
+    return { employeechoice, chosenemployee };
+};
+
+// Update employee's manager.
+// Note: per specifications, an employee can have many managers.
+//       Locgically, a manager can have many employees, I think the assignment is backwards, creating a many-many relationship
+async function update_employee_manager() {
+    console.log("\nCURRENT EMPLOYEE LIST".inverse);
+    await EmployeeTable.show();
+
+    const updatechoice = await select({
+        message: "Select how you want to update an employee's manager",
+        choices: [
+            {
+                name: 'Assign manager',
+                value: 'newmanager',
+                description: 'Assign (add) a new manager to employee (employee can have multiple managers)',
+            },
+            {
+                name: 'Unassign manager',
+                value: 'delmanager',
+                description: 'Unassign (remove) a manager from an employee',
+            },
+            {
+                name: 'Back',
+                value: 'back',
+                description: 'Return to main menu',
+            },
+        ],
+    });
+
+    let employeechoice, chosenemployee, managerchoice, chosenmanager, managersofemployee;
+
+    switch (updatechoice) {
+        case 'newmanager':
+
+            ({ employeechoice, chosenemployee } = await chooseanemployee("Select an employee:\n"));
+            console.log("CHOSEN EMPLOYEE:\n".inverse, chosenemployee);
+
+            // The following object destructuring is a bit unreadable, but the following essentially means:
+            //   managerchoice = (the return value of chooseanemployee).employeechoice
+            //   chosenmanager = (the return value of chooseanemployee).chosenemployee
+            // See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+            ({ employeechoice: managerchoice, chosenemployee: chosenmanager } = await chooseanemployee("Select a manager to assign\n"));
+            console.log("CHOSEN MANAGER:".inverse, managerchoice, chosenmanager);
+
+            // Checks & validations
+            // Check if the employee is already assigned to the manager
+            managersofemployee = (await EmployeeTable.getmanagers(employeechoice)).map((elem) => elem.id);
+            if (managersofemployee.includes(managerchoice)) {
+                console.log(`The employee is already assigned to ${chosenmanager.name}, cancelling manager assignment`.inverse);
+                return;
+            };
+
+            // If the employee and the manager is the same, check with the user
+            if (employeechoice == managerchoice) {
+                if (await confirm({
+                    message: 'Warning: Chosen employee and manager is the same, continue?',
+                    default: false
+                })) {
+                    console.log("Continuing to assigning new manager to employee".inverse);
+                } else {
+                    console.log("Cancelling manager assignment".inverse);
+                    return;
+                };
+            };
+
+            await EmployeeTable.addmanager(employeechoice, managerchoice);
+
+            console.log("\nREFRESHED EMPLOYEE LIST".inverse);
+            await EmployeeTable.show();
+
+            break;
+
+        case 'delmanager':
+
+            ({ employeechoice, chosenemployee } = await chooseanemployee("Select an employee:\n"));
+            console.log("CHOSEN EMPLOYEE:".inverse, employeechoice, chosenemployee);
+
+            // Inquirer prompt to select manageres
+            let managersofemployeechoices = (await EmployeeTable.getmanagers(employeechoice)).map((elem) => { return { name: elem.name, value: elem.id, description: elem.name } });
+
+            // Different cases if employee has no manager, one manager, or multiple managers
+            if (managersofemployeechoices.length === 0) {
+                console.log("Employee has no manager".red);
+                return;
+            } else if (managersofemployeechoices.length === 1) {
+                managerchoice = managersofemployeechoices[0].value;
+            } else if (managersofemployeechoices.length >= 2) {
+                managerchoice = await select(
+                    {
+                        message: "Choose a manager to unassign from this employee",
+                        choices: managersofemployeechoices,
+                        pageSize: managersofemployeechoices.length,
+                        loop: true
+                    });
+            } else {
+                console.log("Error, unexpected condition".red);
+                return;
+            };
+
+            await EmployeeTable.delmanager(employeechoice, managerchoice);
+
+            console.log("\nREFRESHED EMPLOYEE LIST".inverse);
+            await EmployeeTable.show();
+
+            break;
+
+        default:
+    };
+
+    // 
+};
+
+// the main while loop menu for a user to select tasks
 let answer;
-let tblength = 0;
 do {
     answer = await select({
         message: "Select an option:\n",
@@ -118,73 +381,50 @@ do {
 
     switch (answer) {
         case "view_departments":
-            // console.log("DEPARTMENTS");
-            // // await displayTable("department");
-            // await displaySQLQueryTable(`SELECT * FROM department ORDER BY department.id`, "", ['Department Id', 'Department']);
+            // await displayTable("department", "name"); // non-class function
+            console.log("\nVIEW DEPARTMENTS".inverse);
             await DepartmentTable.show();
             break;
 
         case "view_roles":
-            // console.log("ROLES");
-            // // await displayTable("role");
-            // const view_roles_query = `SELECT role.id, role.title, role.salary, department.name FROM role 
-            //                 INNER JOIN department ON role.department_id = department.id ORDER BY role.id;`;
-            // await displaySQLQueryTable(view_roles_query, "", ['Role id', 'Title', 'Salary', 'Department']);
+            console.log("\nVIEW EMPLOYEE ROLES".inverse);
             await RoleTable.show();
             break;
 
         case "view_employees":
-            // console.log("EMPLOYEES");
-            // // await displayTable("employee");
-            // const view_employees_query = `
-            // SELECT emp1.id, 
-            //        emp1.first_name, 
-            //        emp1.last_name, 
-            //        role.title, 
-            //        department.name, 
-            //        role.salary, 
-            //        CONCAT(emp2.first_name, ' ', emp2.last_name) as manager
-            // FROM (((employee AS emp1 INNER JOIN role ON emp1.role_id = role.id) 
-            //                          INNER JOIN department ON role.department_id = department.id) 
-            //                          LEFT JOIN hierarchy ON emp1.id = hierarchy.id) 
-            //                          LEFT JOIN employee AS emp2 ON hierarchy.manager_id = emp2.id ORDER BY emp1.id`;
-            // await displaySQLQueryTable(view_employees_query, "", ['ID', 'First Name', 'Last Name', 'Title', 'Department', 'Salary', 'Manager']);
-            // console.log("TABLE LENGTH:", tblength);
+            console.log("\nVIEW EMPLOYEES".inverse);
             await EmployeeTable.show();
             break;
 
+        case "view_employees_by_manager":
+            console.log("\nVIEW EMPLOYEES BY MANAGER".inverse);
+            await EmployeeTable.show_by_manager();
+            break;
+
+        case "view_employees_of_manager":
+            console.log("\nVIEW EMPLOYEES BY MANAGER".inverse);
+            break;
+
         case "add_department":
-            // display department
-            // console.log("DEPARTMENT");
-            // await displaySQLQueryTable(`SELECT * FROM department ORDER BY department.id`, "", ['Department Id', 'Department']);
-            await DepartmentTable.show();
-
-            const newdeptname = await input({ message: 'Enter new department name' });
-            const query_department_name = `SELECT * FROM department WHERE name = '${newdeptname}';`;
-            const dept_exists = await getSQLQuery(query_department_name, "");
-            if (dept_exists.length > 0) {
-                console.log(`Error: ${newdeptname} department already exists`);
-            } else {
-                try {
-                    await getSQLQuery("INSERT INTO department (name) VALUES (?)", [newdeptname]);
-                } catch (err) {
-                    console.log("Error adding department:", err);
-                };
-            };
-
-            // display department
-            // console.log("DEPARTMENT");
-            // await displaySQLQueryTable(`SELECT * FROM department ORDER BY department.id`, "", ['Department Id', 'Department']);
-            await DepartmentTable.show();
+            await add_department();
             break;
+
         case "add_role":
+            await add_role();
             break;
+
         case "add_employee":
+            await add_employee();
             break;
-        case "septagon":
+
+        case "update_employee_role":
+            await update_employee_role();
             break;
-        case "octagon":
+
+        case "update_employee_manager":
+            await update_employee_manager();
             break;
+
         case "polygon":
             break;
         case "setcolor":
